8e32319b69a98f090ee1db273dfed912e8192e24
juhijj50 <juhijani38@gmail.com>
2024-09-07 01:29:50 +0530

    updated index.js

--- index.js
+++ index.js
@@ -26,12 +26,8 @@ const app = express();
 app.use(express.json());
 app.use(cors);
 
-const port = process.env.PORT || 5000;
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server is running on http://localhost:${port}`);
-  connectDb();
-});
 
+const port = process.env.PORT ;
 
 app.get("/", (req, res) => {
   res.send("Server is working");
